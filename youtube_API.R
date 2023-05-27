d27eb4550327f527be4bbd2a30c6b162ba7a6e89
jjaaccyy <a0970907587@gmail.com>
2023-05-27 23:03:49 +0800

    0527

--- youtube_API.R
+++ youtube_API.R
@@ -2,13 +2,15 @@ library(tuber)
 clientID<-"302334093517-lpoj349umim8sgal3o38gk18omlkfv0f.apps.googleusercontent.com"
 clientKey<-"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"
 yt_oauth(clientID,clientKey,token = " ")
-stat<-get_stats(video_id = "3KMWUUxKOIY")
+stat<-get_stats(video_id = "54PjzjQv1W0")
 data.frame(stat)
-detail<-get_video_details(video_id = "3KMWUUxKOIY")
-str(detail$items)
-
+detail<-get_video_details(video_id = "54PjzjQv1W0")
 str(detail)
-yttitle<-detail$items[[1]]$snippet$title
-comment<-get_all_comments(video_id = "3KMWUUxKOIY")
+View(detail$items[[1]]$snippet)
+tagsName<-data.frame(tags=detail$items[[1]]$snippet$tags)
+
+comment<-get_all_comments(video_id = "54PjzjQv1W0")
 View(data.frame(comment))
 as.numeric(comment$likeCount)->comment$likeCount
+write.table(comment,file="comment.txt",sep = ",",row.names = F,col.names = T)
+saveRDS(comment,"comment.rds")
