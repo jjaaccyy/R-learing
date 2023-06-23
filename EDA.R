ae2d53ba42db9267eb49813a488493fffe9cb416
jjaaccyy <a0970907587@gmail.com>
2023-06-23 10:36:42 +0800

    0622

--- EDA.R
+++ EDA.R
@@ -20,3 +20,19 @@ cor(airquality$Temp,airquality$Wind)
 mtcars
 table(mtcars$cyl,mtcars$gear)->mtcars_gear
 prop.table(mtcars_gear,margin = 1)
+
+grepl(6,mtcars$cyl)->cyl_6
+mtcars$mpg->mpg_extract
+mpg_extract[cyl_6]->cyl_6extract
+grepl(4,mtcars$cyl)->cyl_4
+mpg_extract[cyl_4]->cyl_4extract
+mean(cyl_6)
+mean(cyl_4)
+
+mean(mtcars[mtcars$cyl==4,"mpg"])
+mean(mtcars[mtcars$cyl==6,"mpg"])
+mean(mtcars[mtcars$cyl==8,"mpg"])
+library(dplyr)
+select(iris,starts_with("Sepal"),Species)->select1 #=iris[,c("Species","Sepal.Length","Sepal.Width")]
+select(iris,Petal.Length:Species)->select2
+select(iris,Sepal.Length:"1990",-sort)->select3 #遇到欄位名稱為num時要加""
